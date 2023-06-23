b8a7b8433dd0fe5dffac734c104640dc52989c01
khudadad.rasikh <khudadad.rasikh@imogate.eu>
2023-06-23 16:35:44 +0200

    implement Nextjs built-in language translation

---
+++ get-dictionary.ts
@@ -0,0 +1,17 @@
+import "server-only";
+import { Locale } from "./i18n-config";
+
+const dictionaries = {
+  en: () => import("./dictionaries/en.json").then((module) => module.default),
+  de: () => import("./dictionaries/de.json").then((module) => module.default),
+  cs: () => import("./dictionaries/cs.json").then((module) => module.default),
+};
+
+export const getDictionary = async (locale: Locale) => {
+  if (dictionaries.hasOwnProperty(locale)) {
+    return dictionaries[locale]();
+  } else {
+    // Fallback to Default Languate(English) if the requested language is not available
+    return dictionaries.en();
+  }
+};
