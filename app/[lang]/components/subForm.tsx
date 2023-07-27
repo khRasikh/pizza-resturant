d859877f7cc8dd983b25cc1bd10240067e42f37c
khudadad.rasikh <khudadad.rasikh@imogate.eu>
2023-07-27 09:24:12 +0200

    chore: cleaning the code

--- app/[lang]/components/subForm.tsx
+++ app/[lang]/components/subForm.tsx
@@ -1,23 +1,10 @@
 "use client";
-import { useContext } from "react";
-import { ValueContext } from "../page";
 
-export default function Panel({
-  title,
-  children,
-}: {
-  title: string;
-  children: React.ReactNode;
-}) {
-  const getvalue = useContext(ValueContext)!;
-  console.log(getvalue.val);
+export default function Panel({ title, children }: { title: string; children: React.ReactNode }) {
   return (
     <section className="bg-gray-100 p-4 rounded-lg">
       <h1 className="text-2xl mb-4">{title}</h1>
-      <h1 className="text-2xl mb-4">
-        this value comes from Parent to grand child:
-        <p className="text-green-800">{getvalue.val}</p>
-      </h1>
+      <h1 className="text-2xl mb-4">this value comes from Parent to grand child:</h1>
       <div className="space-y-2">{children}</div>
     </section>
   );
