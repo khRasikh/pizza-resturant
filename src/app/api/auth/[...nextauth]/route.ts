c6eb70a7315f4665550ff1ef1ae325f3c86a577d
khRasikh <kh.rasikh542@gmail.com>
2023-11-18 23:11:51 +0100

    fixing error in route.ts file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,17 +4,17 @@
 // const handler = NextAuth(config);
 // export { handler as GET, handler as POST };
  
+import { AuthOptions } from "next-auth";
 import NextAuth from "next-auth/next"
 import GoogleProvider from "next-auth/providers/google";
 
 
-export const authOptions = {
+export const authOptions: AuthOptions  = {
     // Configure one or more authentication providers
     providers: [
       GoogleProvider({
-        clientId: process.env.GOOGLE_ID || "",
-        clientSecret: process.env.GOOGLE_SECRET || "",
-        
+        clientId: process.env.GOOGLE_ID as string,
+        clientSecret: process.env.GOOGLE_SECRET as string,
       }),
     ],
   };
