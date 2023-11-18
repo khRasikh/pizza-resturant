ca164f3e93098704726b225a1894c374da165df3
khRasikh <kh.rasikh542@gmail.com>
2023-11-18 23:55:02 +0100

    removing unnecessary commands

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,3 @@
-// import NextAuth from "next-auth/next";
-// import { config } from "auth";
-
-// const handler = NextAuth(config);
-// export { handler as GET, handler as POST };
- 
 import { authOptions } from "@/components/auth/auth";
 import NextAuth from "next-auth/next"
 
