4e3039bc15270f9eba367dea71ffb3120447e521
kh.rasikh542@gmail.com <khudadad.rasikh@imogate.eu>
2023-11-21 17:32:16 +0100

    adding sign in feature using credentials

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,28 +1,9 @@
 "use client";
 
 import PageLayout from "@/components/PageLayout";
+import { fallback, logError } from "@/components/shared/customError";
 import { useTranslations } from "next-intl";
 import { ErrorBoundary } from "react-error-boundary";
-
-function fallback({ error, resetErrorBoundary }: any) {
-  // Call resetErrorBoundary() to reset the error boundary and retry the render.
-
-  return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre style={{ color: "red" }}>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
-  );
-}
-
-
-const logError = (error: Error, info: { componentStack: string }) => {
-  // Do something with the error, e.g. log to an external API
-  console.log("test Do something with the error, e.g. log to an external API")
-};
-
-
 
 export default function IndexPage() {
   const t = useTranslations("IndexPage");
