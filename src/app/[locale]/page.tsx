b1e5461fb8c6c0db0f6e7b5d84d1770f5970796e
khRasikh <kh.rasikh542@gmail.com>
2023-12-04 21:55:31 +0100

    updating minor changes on Cached data

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,22 +5,21 @@ import { fallback, logError } from "@/components/shared/customError";
 import { useTranslations } from "next-intl";
 import { ErrorBoundary } from "react-error-boundary";
 
+export const fetchCache = "force-no-store";
+
 export default function IndexPage() {
   const t = useTranslations("IndexPage");
 
   return (
-    <ErrorBoundary FallbackComponent={fallback} onError={logError} >
+    <ErrorBoundary FallbackComponent={fallback} onError={logError}>
       <PageLayout title={t("title")}>
         <p className="max-w-[590px]">
           {t.rich("description", {
             p: (chunks) => <p className="mt-4">{chunks}</p>,
-            code: (chunks) => (
-              <code className="font-mono text-green-500">{chunks}</code>
-            ),
+            code: (chunks) => <code className="font-mono text-green-500">{chunks}</code>,
           })}
         </p>
       </PageLayout>
     </ErrorBoundary>
-
   );
-} 
+}
