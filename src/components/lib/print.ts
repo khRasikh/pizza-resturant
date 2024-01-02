78f5a70f5fdac2a023592f3af987bb05731e3f1c
kh.rasikh542@gmail.com <khRasikh>
2024-01-02 22:27:16 +0100

    changing a4 to a5 print size

--- src/components/lib/print.ts
+++ src/components/lib/print.ts
@@ -1,17 +1,7 @@
 import { IPrint } from "../interface/general";
 import { formatNumber } from "../shared/constants";
 import { formattedDate } from "./customDate";
-// {
-//     "id": "6",
-//     "customer_id": "1352",
-//     "name": "CALZONE",
-//     "price": "9.99",
-//     "count": 22,
-//     "extra": 1,
-//     "discount": 22,
-//     "total": 219.78,
-//     "order_date": "Mon Jan 01 2024 12:07:34 GMT+0100 (Central European Standard Time)"
-// }
+
 export const handlePrint = ({ customer, orderList, toggleModal }: IPrint) => {
   let totalPrices = 0;
 
@@ -19,7 +9,6 @@ export const handlePrint = ({ customer, orderList, toggleModal }: IPrint) => {
   const orderItemsHTML = orderList
     .map((order) => {
       totalPrices += Number(order.total); // Calculate totalPrices
-      //   totalPrices += formatNumber(parseInt(order.total)); // Calculate totalPrices
 
       return `
             <tr>
@@ -36,81 +25,99 @@ export const handlePrint = ({ customer, orderList, toggleModal }: IPrint) => {
 
   // HTML content to be printed
   const content = `
-        <html>
-        <head>
-            <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-        </head>
-        <body class="bg-white">
-            <div class="bill-container mx-auto  rounded-md ">
-                <!-- First Row - Logo -->
-                <div class="flex justify-center mb-4">
-                    <img src="logo.png" alt="Logo" class="w-32 h-32" />
-                </div>
-
-                <!-- Second Row - Data and Bill No -->
-                <div class="flex justify-between mb-4">
-                    <div>${formattedDate()}</div>
-                    <div>Nr: ${orderList[0].id}</div>
-                </div>
-
-                <div class="flex flex-col">
-                    <div class="flex justify-between mb-1">
-                        <div>KNr: ${customer.KNr}</div>
-                        <div>Tel: ${customer.Tel}</div>
-                    </div>
-                    <div class="flex justify-between mb-1">
-                        <div>${customer.Str}</div>
-                        <div>${customer.Seit}</div>
-                        <div>${customer.Ort}</div>
-                    </div>
-                </div>
-
-
-                <!-- Fourth Row - Dash -->
-                <div class="divider my-4 border-b border-dashed border-gray-500"></div>
-
-                <!-- Fifth Row - Items List -->
-                <div class="items-list">
-                    <table class="w-full">
-                        <thead>
-                            <tr>
-                                <th>Anz</th>
-                                <th>Nr.</th>
-                                <th>Bez.</th>
-                                <th>Extra</th>
-                                <th>Pr</th>
-                                <th>%MW</th>
-                            </tr>
-                        </thead>
-                        <tbody class="text-center">
-                            ${orderItemsHTML} <!-- Inject the generated order items HTML -->
-                        </tbody>
-                    </table>
-                </div>
-
-                <!-- Sixth Row - Dash -->
-                <div class="divider my-4 border-b border-dashed border-gray-500"></div>
-
-                <!-- Seventh Row - Total -->
-                <div class="flex justify-between">
-                    <div>Rechnungsbetrag Brutto:</div>
-                    <div class="font-bold">€ ${totalPrices.toFixed(2)}</div>
-                </div>
-
-                <div class="flex justify-between mt-4 mb-2">
-                    <div>Steuer-Nr: 144/126/80559</div>
-                </div>
-
-                <div class="flex justify-between mt-4 ">
-                    <div>Vielen Dank Für Ihre Bestellung</div>
-                </div>
-
-                <div class="flex justify-between mt-4 ">
-                <div></div>
-
+    <html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+        <style>
+            @media print {
+                @page {
+                    size: A5; /* Set the page size to A5 */
+                }
+                
+                /* Additional styles for better print layout if needed */
+                body {
+                    font-family: Arial, sans-serif; /* Example: Change font-family for better readability */
+                }
+                
+                /* Define other print-specific styles if needed */
+                /* For example, hiding certain elements from print */
+                .no-print {
+                    display: none;
+                }
+            }
+        </style>
+    </head>
+    <body class="bg-white">
+        <div class="bill-container mx-auto  rounded-md ">
+            <!-- First Row - Logo -->
+            <div class="flex justify-center mb-4">
+                <img src="logo.png" alt="Logo" class="w-32 h-32" />
             </div>
-        </body>
-        </html>
+
+            <!-- Second Row - Data and Bill No -->
+            <div class="flex justify-between mb-4">
+                <div>${formattedDate()}</div>
+                <div>Nr: ${orderList[0].id}</div>
+            </div>
+
+            <div class="flex flex-col">
+                <div class="flex justify-between mb-1">
+                    <div>KNr: ${customer.KNr}</div>
+                    <div>Tel: ${customer.Tel}</div>
+                </div>
+                <div class="flex justify-between mb-1">
+                    <div>${customer.Str}</div>
+                    <div>${customer.Seit}</div>
+                    <div>${customer.Ort}</div>
+                </div>
+            </div>
+
+
+            <!-- Fourth Row - Dash -->
+            <div class="divider my-4 border-b border-dashed border-gray-500"></div>
+
+            <!-- Fifth Row - Items List -->
+            <div class="items-list">
+                <table class="w-full">
+                    <thead>
+                        <tr>
+                            <th>Anz</th>
+                            <th>Nr.</th>
+                            <th>Bez.</th>
+                            <th>Extra</th>
+                            <th>Pr</th>
+                            <th>%MW</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-center">
+                        ${orderItemsHTML} <!-- Inject the generated order items HTML -->
+                    </tbody>
+                </table>
+            </div>
+
+            <!-- Sixth Row - Dash -->
+            <div class="divider my-4 border-b border-dashed border-gray-500"></div>
+
+            <!-- Seventh Row - Total -->
+            <div class="flex justify-between">
+                <div>Rechnungsbetrag Brutto:</div>
+                <div class="font-bold">€ ${totalPrices.toFixed(2)}</div>
+            </div>
+
+            <div class="flex justify-between mt-4 mb-2">
+                <div>Steuer-Nr: 144/126/80559</div>
+            </div>
+
+            <div class="flex justify-between mt-4 ">
+                <div>Vielen Dank Für Ihre Bestellung</div>
+            </div>
+
+            <div class="flex justify-between mt-4 ">
+                <div></div>
+            </div>
+        </div>
+    </body>
+    </html>
     `;
 
   // Open a new window to print the content
